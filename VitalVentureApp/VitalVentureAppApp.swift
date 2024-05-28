a2ac6195bf07052db2359bf9672cc52995f704ec
kamilkeben <kmlkbn@gmail.com>
2024-05-28 16:59:45 +0300

    Initial Commit

---
+++ VitalVentureApp/VitalVentureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitalVentureAppApp.swift
+//  VitalVentureApp
+//
+//  Created by KamilKeben on 28.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct VitalVentureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
